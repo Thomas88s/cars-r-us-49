2260f7a25e03bc96ccee65fb87a7b8b7d02857f0
Thomas Witherow <twitherow@bellsouth.net>
2021-06-08 08:49:27 -0500

    haveing an issue with the price

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,16 +1,67 @@
 
-    import { getOrders } from "./database.js"
+    import { getOrders, getColors, getInteriors, getTechnology, getWheels } from "./database.js"
+    
 
     const orders = getOrders()
+    const colors = getColors()
+    const interiors = getInteriors()
+    const technology = getTechnology()
+    const wheels = getWheels()
 
+    const foundColor = colors.find(
+        (color) => {
+            for (let order of orders) {
+                 return color.id === order.colorId
+            } 
+        }  
+    ) 
+    const foundInterior = interiors.find(
+        (interior) => {
+            for (let order of orders) {
+                 return interior.id === order.interiorId
+            } 
+        } 
+    )
+    const foundTechnology = technology.find(
+        (technology) => {
+            for (let order of orders) {
+                 return technology.id === order.technologyId
+            } 
+        } 
+    )
+    const foundWheels = wheels.find(
+        (wheel) => {
+            for (let order of orders) {
+             
+                 return wheel.id === order.wheelsId
+            
+            } 
+        }
+        
+    )
+    
+  
+    const colorCost = foundColor.price 
+    const interiorCost = foundInterior.price
+    const technologyCost = foundTechnology.price
+    const wheelsCost = foundWheels.price    
+
+    const totalCost = colorCost + interiorCost + technologyCost + wheelsCost
+
+    console.log(totalCost)
+
+    const costString = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+    })
 
     const buildOrderListItem = (order) => {
         return `<li>
-            Order #${order.id} was placed on ${order.timestamp}
+            Order #${order.id} was placed on ${order.timestamp} and has a total cost of ${costString}
         </li>`
     }
     
-    export const Orders = (order) => {
+    export const Orders = () => {
         /*
             Can you explain why the state variable has to be inside
             the component function for Orders, but not the others?
